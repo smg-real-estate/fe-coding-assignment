34792c79757037ebcb0a19dc53d25312d286d1f1
Minh Huynh <minh.huynh@swissmarketplace.group>
2026-01-02 16:22:16 +0700

    chore: updated base url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://2yahugzd4a.execute-api.eu-central-1.amazonaws.com/test'; // hosted in SA test account
+const BASE_URL = 'https://amgmqmmo4b.execute-api.eu-central-1.amazonaws.com/prod'; // hosted in aws_re-srp-test
 
 export function getListings() {
   return axios(`${BASE_URL}/listings`);
